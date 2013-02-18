265f7565e7374fcc06633f587e39701047ac6c10
Matthew Chase Whittemore <mcwhittemore@gmail.com>
2013-02-18 12:20:12 -0500

    added .html and .css to the gist object in github.js and showed it working in localhost:3000/test

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ app.configure('development', function(){
 });
 
 app.get('/', function(req, res){ routes.index(req, res, app.github); });
+app.get('/test', function(req, res){ routes.test(req, res, app.github); });
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -5,19 +5,25 @@
 exports.index = function(req, res, github){
   	
   	if(github.isLoaded){
-  		var posts = github.getCategoryPosts("Blog");
-  		console.log(posts);
+  		var posts = github.getGroupPosts("Blog");
   		var post = github.getPost(posts[0]);
-  		console.log(post);
   		res.render('index', post);
   	}
   	else{
   		exports.index(req, res, github);
   	}
-
 };
 
 exports.list = function(req, res){
   res.send("respond with a resource");
 };
+
+exports.test = function(req, res, github){
+
+  var posts = github.getGroupPosts("Blog");
+  var post = github.getPost(posts[0]);
+  
+  res.send(post.css+"\n"+post.html);
+
+}
 
